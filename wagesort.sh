dc4626cdd031a42db6b9b53042c221107a480d8b
Madeleine Girgis <madeleinecgirgis@gmail.com>
2019-10-10 18:04:48 -0400

    Added next three steps

--- wagesort.sh
+++ wagesort.sh
@@ -1,3 +1,5 @@
 
 cat wages.csv | cut -d , -f 1,2 | sed 's/,/ /g' | grep "male" | sort --version-sort | uniq >> genderyears.csv
-
+cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | grep "male" | sort -k3 --version-sort | tail -n 1 
+cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | grep "male" | sort -k3 --version-sort | head -n 1 
+cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | grep "male" | sort -k3 --version-sort | tail -n 10 | grep "female" | wc -l  
